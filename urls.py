cbc6b53eb6d91fefac7f523d9841ee5ec51857c6
vaibhav patel <118597690+vaibhvapatel18@users.noreply.github.com>
2023-09-02 16:10:35 +0530

    Add files via upload
    gb

--- urls.py
+++ urls.py
@@ -1,27 +1,23 @@
+"""
+URL configuration for authproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path,include
-from apiapp.views import UserRegistrationView,UserLoginView,UserProfileView,UserChangePasswordView,SendPasswordEmailView,Restaurantadd,EmailLinkPasswordResetView,Restaurantmanuadd,UserlikeRestaurant,Userlikemenuitem,UserlikeRestaurant,Usersavemenuitem
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-
-router.register('restaurantadd', Restaurantadd, basename='restaurantlist')
-router.register('restaurantaddmenu', Restaurantmanuadd, basename='restaurantaddmenu')
-# router.register('userlikerestaurant', UserlikeRestaurant, basename='userlikerestaurant')
-# router.register('userlikemenuitem', Userlikemenuitem, basename='userlikemenuitem')
-
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name = 'register'),
-    path('login/', UserLoginView.as_view(), name = 'login'),
-    path('profile/', UserProfileView.as_view(), name = 'profile'),
-    path('changepassword/', UserChangePasswordView.as_view(), name = 'changepassword'),
-    path('resetpasswordlink/', SendPasswordEmailView.as_view(), name = 'resetpasswordlink'),
-    path('resetpasswordemail/<uid>/<token>/', EmailLinkPasswordResetView.as_view(), name='resetpasswordemail'),
-    path('userlikerestaurant/', UserlikeRestaurant.as_view(), name='userlikerestaurant'),
-    path('userlikemenuitem/', Userlikemenuitem.as_view(), name='userlikemenuitem'),
-    path('usersavemenuitem/', Usersavemenuitem.as_view(), name='usersavemenuitem'),
-    # path('userunlikerestaurant/', UserunlikeRestaurant.as_view(), name='userunlikerestaurant/'),
-    path('',include(router.urls)),
-
+    path('admin/', admin.site.urls),
+    path('',include('apiapp.urls'))
 ]
